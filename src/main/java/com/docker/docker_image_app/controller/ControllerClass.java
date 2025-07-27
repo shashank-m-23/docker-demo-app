497a7899105dcffc9402454fdad54d00e7bd0c92
shashank-m-23 <shashankm680@gmail.com>
2025-07-27 20:19:39 +0530

    changed the controller path

--- src/main/java/com/docker/docker_image_app/controller/ControllerClass.java
+++ src/main/java/com/docker/docker_image_app/controller/ControllerClass.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="/docker")
+@RequestMapping(value="/docker/v1")
 public class ControllerClass 
 {
 	@GetMapping(value="/getMessage")
